816bbedb3008cdd3320491a47e4806b2f62ff4aa
Tantanok221 <6572withspicy@gmail.com>
2023-10-14 22:16:39 +0800

    Updated main.js about API Code

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 import './main.sass'
 import FetchWrapper from './lib/fetchWrapper'
-var fromUnixTime = ('date-fns/fromUnixTime')
+import {fromUnixTime,format} from 'date-fns'
 
 const API = new FetchWrapper(`https://api.openweathermap.org/data/2.5/weather?id=1733046&appid=`) 
+const DOM = {
+    temperature: document.querySelector(".temperature"),
+    time: document.querySelector(".time"),
+    speed: document.querySelector(".speed"),
+    pressure: document.querySelector(".pressure"),
+    humidity: document.querySelector(".humidity"),
+    icon: document.querySelector(".weather")
 
+}
 try {
-    const data = await API.get(key.KEY)
-    console.log(data)
-    // const icon = data.weather[0].main
-    const temperature = data.main.temp + 273.15
-    const humdity = data.main.humidity
-    const pressure = data.main.pressure
-    const speed = data.wind.speed
-    const time = fromUnixTime(data.dt)
-    console.log(time)
+    var data = await API.get(key.KEY)   
 }catch(error)
 {
     console.error(error)
 }
+console.log(data)
+const icon = data.weather[0].main
+const temperature = (data.main.temp - 273.15).toFixed(1) 
+const humidity = data.main.humidity
+const pressure = data.main.pressure
+const speed = data.wind.speed
+const time = format(fromUnixTime(data.dt),"dd MMM yyyy, h:m a")
+
+DOM.temperature.textContent = String(temperature + " °C")
+DOM.time.textContent = time
+DOM.speed.textContent = speed
+DOM.pressure.textContent = String(pressure + " hPa") 
+DOM.humidity.textContent = String(humidity+"%")
