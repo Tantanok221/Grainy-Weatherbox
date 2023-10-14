5868d5897f2855e0b8e1ec0452abe6a92630d993
Tan Kai <82860997+Tantanok221@users.noreply.github.com>
2023-10-14 21:23:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 import './main.sass'
 import FetchWrapper from './lib/fetchWrapper'
+var fromUnixTime = ('date-fns/fromUnixTime')
 
 const API = new FetchWrapper(`https://api.openweathermap.org/data/2.5/weather?id=1733046&appid=`) 
 
 try {
-    // const data = API.get(key.KEY).then(data => data)
-    // console.log(data)
-    
-
+    const data = await API.get(key.KEY)
+    console.log(data)
+    // const icon = data.weather[0].main
+    const temperature = data.main.temp + 273.15
+    const humdity = data.main.humidity
+    const pressure = data.main.pressure
+    const speed = data.wind.speed
+    const time = fromUnixTime(data.dt)
+    console.log(time)
 }catch(error)
 {
     console.error(error)
