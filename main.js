ddfa5396349b2aa954c37df61ec25bedf88ef222
Tantanok221 <6572withspicy@gmail.com>
2023-10-15 11:43:51 +0800

    Refactor SASS/JS Code, Added logic code that change icon depend on the weather

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
-import './main.sass'
+import './sass/main.sass'
 import FetchWrapper from './lib/fetchWrapper'
 import {fromUnixTime,format} from 'date-fns'
+import {DOM} from "./lib/DOM"
+import UpdateWeather from './lib/updateWeather'
 
 const API = new FetchWrapper(`https://api.openweathermap.org/data/2.5/weather?id=1733046&appid=`) 
-const DOM = {
-    temperature: document.querySelector(".temperature"),
-    time: document.querySelector(".time"),
-    speed: document.querySelector(".speed"),
-    pressure: document.querySelector(".pressure"),
-    humidity: document.querySelector(".humidity"),
-    icon: document.querySelector(".weather")
 
-}
+
+
+
 try {
     // var data = await API.get(key.KEY)   
 }catch(error)
 {
     console.error(error)
 }
-console.log(data)
-const icon = data.weather[0].main
+
+
+const icon = data.weather[0].id
 const temperature = (data.main.temp - 273.15).toFixed(1) 
 const humidity = data.main.humidity
 const pressure = data.main.pressure
@@ -31,3 +29,4 @@ DOM.time.textContent = time
 DOM.speed.textContent = speed
 DOM.pressure.textContent = String(pressure + " hPa") 
 DOM.humidity.textContent = String(humidity+"%")
+UpdateWeather(icon,DOM.icon)
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ This is a small project that I've used to experiment with grainy texture and API
 2. Run ```npm install```
 3. Run ```npm run dev```
 4. Open the Local Host File and have fun!
+
