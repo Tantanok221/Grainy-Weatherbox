99315be7e69029cc5ae88ae69e3607a950251dca
Tantanok221 <6572withspicy@gmail.com>
2023-10-17 08:44:13 +0800

    Added datalabels to the graph

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import DateFilter from "./lib/helper/DateFilter"
 import getData from './lib/getData'
 import formatDate from "./lib/formatDate"
 import Chart from 'chart.js/auto'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
 
 
 function mapData(arrayx,arrayy,step){
@@ -44,18 +45,38 @@ const config = {
             enabled: false
         },
         
+        
     },
     
         
     }
 }
+Chart.register(ChartDataLabels)
 Chart.defaults.plugins.legend.display = false
 Chart.defaults.font.size = 24
 Chart.defaults.font.family = "Rajdhani"
 Chart.defaults.font.weight = "Regular"
 Chart.defaults.color = '#fff'
 Chart.defaults.borderColor = '#fff'
-new Chart(document.getElementById("chart"),config)
+Chart.defaults.plugins.datalabels.color = "#fff"
+Chart.defaults.plugins.datalabels.font.size = "13"
+Chart.defaults.plugins.datalabels.font.family = "Rajdhani"
+Chart.defaults.plugins.datalabels.backgroundColor = "#000"
+Chart.defaults.plugins.datalabels.align = "end"
+Chart.defaults.plugins.datalabels.formatter = function(value,context) {
+    return value + " °C"
+}
+Chart.defaults.plugins.datalabels.borderRadius = 8
+Chart.defaults.plugins.datalabels.padding.left = 6
+Chart.defaults.plugins.datalabels.padding.right = 6
+const mychart = new Chart(document.getElementById("chart"),config)
+window.addEventListener('beforeprint', () => {
+    mychart.resize(600, 600);
+  });
+  window.addEventListener('afterprint', () => {
+    mychart.resize();
+  });
+
 
 // const ChartData = mapData(futureForecast.time,futureForecast.temperature,8)
 
