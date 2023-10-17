8bbf8e825de77f74ef1d1a05fb8d569bdfccfcf7
Tantanok221 <6572withspicy@gmail.com>
2023-10-17 15:27:21 +0800

    Completed Five Day Icon and Data Load In Part

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import './sass/main.sass'
 import FetchWrapper from './lib/helper/fetchWrapper'
-import {fromUnixTime,format} from 'date-fns'
+import {format} from 'date-fns'
 import init from "./lib/init"
 import DateFilter from "./lib/helper/DateFilter"
 import getData from './lib/getData'
 import formatDate from "./lib/helper/formatDate"
 import drawGraph from './lib/drawGraph'
-
+import UpdateWeather from './lib/helper/updateWeather'
 
 
 
@@ -15,15 +15,26 @@ const futureForecast = {
     time: DateFilter(DATA,1,8).map(data =>  formatDate(data.time).substring(13)), // X Axis
     temperature: DateFilter(DATA,1,8).map(data =>  data.temperature) //Data Point
 }  
-console.log(futureForecast.time)
 drawGraph(futureForecast.time,futureForecast.temperature)
 
 
 
 // init()
+const FiveDay = {
+    icon: DateFilter(DATA,8,5).map(data =>  data.icon), 
+    date: DateFilter(DATA,8,5).map(data =>  format(data.time,"dd MMM")),
+    DOW: DateFilter(DATA,8,5).map(data =>  format(data.time,"EEE").toUpperCase())
+}  // Filter future five date(include today) data based on the user time
+const DOMFiveDay ={
+    icon: document.querySelectorAll(".icon"),
+    date: document.querySelectorAll(".text"),
+    DOW: document.querySelectorAll(".b") // Select all Day of Week Indicator
+} 
+for(let i = 0;i < DOMFiveDay.icon.length; i++){
+    UpdateWeather(FiveDay.icon[i],DOMFiveDay.icon[i])
+    DOMFiveDay.date[i].textContent = FiveDay.date[i]
+    if(i != 0) DOMFiveDay.DOW[i].textContent = FiveDay.DOW[i] 
+}
 
-
-
-const FiveDayIcon = DateFilter(DATA,8,5).map(data =>  data.icon) // Filter future five date(include today) data based on the user time
 
 
