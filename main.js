73626913350beed15475f55501c6e03196b33b02
Tantanok221 <6572withspicy@gmail.com>
2023-10-17 17:30:41 +0800

    Added Responsiveness to the graph and fix the gradient and grainy texture scale

--- main.js
+++ main.js
@@ -7,34 +7,59 @@ import getData from './lib/getData'
 import formatDate from "./lib/helper/formatDate"
 import drawGraph from './lib/drawGraph'
 import UpdateWeather from './lib/helper/updateWeather'
+import {DOM,DOMFiveDay} from "./lib/helper/DOM"
 
-
-
+let chart;
+// Computer Window Size
 const DATA = await getData()
-const futureForecast = {
-    time: DateFilter(DATA,1,8).map(data =>  formatDate(data.time).substring(13)), // X Axis
-    temperature: DateFilter(DATA,1,8).map(data =>  data.temperature) //Data Point
-}  
-drawGraph(futureForecast.time,futureForecast.temperature)
+if(window.innerWidth >= 640) {
+    const futureForecast = {
+        time: DateFilter(DATA,1,8).map(data =>  formatDate(data.time).substring(13)), // X Axis
+        temperature: DateFilter(DATA,1,8).map(data =>  data.temperature) //Data Point
+    }  
+    chart = drawGraph(futureForecast.time,futureForecast.temperature)
+    
+}else {
+    const futureForecast = {
+        time: DateFilter(DATA,1,4).map(data =>  formatDate(data.time).substring(13)), // X Axis
+        temperature: DateFilter(DATA,1,4).map(data =>  data.temperature) //Data Point
+    }  
+    chart = drawGraph(futureForecast.time,futureForecast.temperature)
+}
 
 
 
-// init()
+init()
 const FiveDay = {
     icon: DateFilter(DATA,8,5).map(data =>  data.icon), 
     date: DateFilter(DATA,8,5).map(data =>  format(data.time,"dd MMM")),
     DOW: DateFilter(DATA,8,5).map(data =>  format(data.time,"EEE").toUpperCase())
 }  // Filter future five date(include today) data based on the user time
-const DOMFiveDay ={
-    icon: document.querySelectorAll(".icon"),
-    date: document.querySelectorAll(".text"),
-    DOW: document.querySelectorAll(".b") // Select all Day of Week Indicator
-} 
+
 for(let i = 0;i < DOMFiveDay.icon.length; i++){
     UpdateWeather(FiveDay.icon[i],DOMFiveDay.icon[i])
     DOMFiveDay.date[i].textContent = FiveDay.date[i]
     if(i != 0) DOMFiveDay.DOW[i].textContent = FiveDay.DOW[i] 
 }
 
-
-
+window.addEventListener("resize", () => {
+    chart.destroy()
+    if(window.innerWidth >= 640) {
+        const futureForecast = {
+            time: DateFilter(DATA,1,8).map(data =>  formatDate(data.time).substring(13)), // X Axis
+            temperature: DateFilter(DATA,1,8).map(data =>  data.temperature) //Data Point
+        }  
+        chart = drawGraph(futureForecast.time,futureForecast.temperature)
+        
+    }else {
+        const futureForecast = {
+            time: DateFilter(DATA,1,4).map(data =>  formatDate(data.time).substring(13)), // X Axis
+            temperature: DateFilter(DATA,1,4).map(data =>  data.temperature) //Data Point
+        }  
+        chart = drawGraph(futureForecast.time,futureForecast.temperature)
+    }
+    DOM.grainy.style.height = DOM.body.offsetHeight + "px"
+    DOM.blackbg.style.height = DOM.body.offsetHeight + "px"
+})
+DOM.blackbg.style.height = DOM.body.offsetHeight + "px"
+DOM.grainy.style.height = DOM.body.offsetHeight + "px"
